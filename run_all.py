19963e56471f5102c3d2b4897478cb309133f367
fangxinforever <41461439+fangxinforever@users.noreply.github.com>
2023-10-19 23:20:34 +0800

    Update run_all.py
    test git conflict

--- run_all.py
+++ run_all.py
@@ -1,4 +1,4 @@
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(["-s", "-v", "test_cases", "--alluredir=test_results/reports/allure-results"])
+    pytest.main(["-s", "-v", "test_cases/api", "--alluredir=test_results/reports/allure-results"])
